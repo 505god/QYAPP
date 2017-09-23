df5d9441e617f465cc8aeb577b2026c0bf746230
qiu <18915410342@126.com>
2017-09-23 14:27:28 +0800

    Initial Commit

---
+++ QYAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QYAPP
+//
+//  Created by 邱成西 on 2017/9/23.
+//  Copyright © 2017年 邱大侠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QYAPP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QYAPP
+//
+//  Created by 邱成西 on 2017/9/23.
+//  Copyright © 2017年 邱大侠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
